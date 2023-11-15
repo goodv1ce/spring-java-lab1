faaa8e1870f914bc470632efbc28c5a98efb2f3a
goodv1ce <andriy.lysak.2003@gmail.com>
2023-11-15 02:53:08 +0200

    [ADD] Fake database implementation bean

---
+++ src/main/java/ua/goodvice/javaspringlab/JavaSpringLab2Application.java
@@ -0,0 +1,20 @@
+package ua.goodvice.javaspringlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ua.goodvice.javaspringlab.util.FakeDatabaseImplementation;
+
+@SpringBootApplication
+public class JavaSpringLab2Application {
+
+    @Bean
+    public FakeDatabaseImplementation fakeDatabaseImplementation() {
+        return new FakeDatabaseImplementation();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringLab2Application.class, args);
+    }
+
+}
