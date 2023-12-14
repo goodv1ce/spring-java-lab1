802ed4cf64730bed2894e224329b15231fe31535
goodv1ce <andriy.lysak.2003@gmail.com>
2023-12-14 19:43:50 +0200

    [DELETE] little fixes

--- src/main/java/ua/goodvice/javaspringlab/JavaSpringLab2Application.java
+++ src/main/java/ua/goodvice/javaspringlab/JavaSpringLab2Application.java
@@ -2,6 +2,7 @@ package ua.goodvice.javaspringlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import ua.goodvice.javaspringlab.util.FakeDatabaseImplementation;
 
